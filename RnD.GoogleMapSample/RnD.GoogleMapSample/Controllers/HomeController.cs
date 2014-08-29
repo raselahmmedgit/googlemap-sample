0f235a18f6e97336300e456956b7ed5d30a8ac32
Rasel Ahmmed Bappi <rasel.bappi@hotmail.com>
2014-08-29 00:59:37 -0700

    2014-08-29

--- RnD.GoogleMapSample/RnD.GoogleMapSample/Controllers/HomeController.cs
+++ RnD.GoogleMapSample/RnD.GoogleMapSample/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RnD.GoogleMapSample.Models;
 
 namespace RnD.GoogleMapSample.Controllers
 {
@@ -10,10 +11,62 @@ namespace RnD.GoogleMapSample.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
             return View();
         }
 
+        public ActionResult Basic()
+        {
+            return View();
+        }
+
+        public ActionResult Load()
+        {
+            return View();
+        }
+
+        public ActionResult LoadDdl()
+        {
+            return View();
+        }
+
+        public JsonResult GetDataList(int id)
+        {
+            var googleMapViewModelList = new List<GoogleMapViewModel>
+                            {
+                                new GoogleMapViewModel { Latitude = "24.100000000000", Longitude ="89.233333330000", Message = "Pabna, Atgharia, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "24.083333330000", Longitude ="89.616666670000", Message = "Pabna, Bera, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "23.327500000000", Longitude ="89.495833330000", Message = "Pabna, Bhangura, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "24.230000000000", Longitude ="89.300000000000", Message = "Pabna, Chatmohar, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "23.600000000000", Longitude ="89.833333330000", Message = "Pabna, Faridpur, Bangladesh."}
+                            };
+
+            return Json(googleMapViewModelList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetDistrictList(int id)
+        {
+            var googleMapViewModelList = new List<GoogleMapViewModel>
+                            {
+                                new GoogleMapViewModel { Latitude = "23.710277780000", Longitude ="90.407222220000", Message = "Dhaka, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "23.833333330000", Longitude ="88.916666670000", Message = "Kushtia, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "22.333333330000", Longitude ="90.333333330000", Message = "Patuakhali, Bangladesh."}
+                            };
+
+            return Json(googleMapViewModelList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetUpazilaList(int id)
+        {
+            var googleMapViewModelList = new List<GoogleMapViewModel>
+                            {
+                                new GoogleMapViewModel { Latitude = "24.100000000000", Longitude ="89.233333330000", Message = "Pabna, Atgharia, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "24.083333330000", Longitude ="89.616666670000", Message = "Pabna, Bera, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "23.327500000000", Longitude ="89.495833330000", Message = "Pabna, Bhangura, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "24.230000000000", Longitude ="89.300000000000", Message = "Pabna, Chatmohar, Bangladesh."},
+                                new GoogleMapViewModel { Latitude = "23.600000000000", Longitude ="89.833333330000", Message = "Pabna, Faridpur, Bangladesh."}
+                            };
+
+            return Json(googleMapViewModelList, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ RnD.GoogleMapSample/RnD.GoogleMapSample/Models/GoogleMapViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RnD.GoogleMapSample.Models
+{
+    public class GoogleMapViewModel
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Message { get; set; }
+    }
+}
